67eb04a4398615d7454ee51c0578afd648672c72
sabihalipy <lipysabiha@gmail.com>
2021-05-04 22:08:14 +0600

    js

--- js/deepfunction1&2.js
+++ js/deepfunction1&2.js
@@ -1,12 +1,12 @@
-// Function Expressions - for making function Expression it should have a variable like example no.2: and without variable it calls Functional Statement like example no.1;
+// Function Expressions - for making function Expression it should have a variable(here variable name is="subtract") like example no.2(line no.07); and without variable it calls Functional Statement like example no.1(line no.3-5);
 
     function add(x, y) {
         return x + y;
     }; // see the console by = add(5, 5) => function statement
-
+    
     const subtract =  function(x, y) {
         return x - y;
-    };  // => function statement
+    };  // => function Expression
 
     const increase = function multiply(x, y) {
         return x * y;
@@ -80,7 +80,9 @@ console.log(calculator.increase(50, 5));
 
     console.log(randomPick(hi, bye));
 
-    // Return of function -from DOM Deep Look Function part-02;
+// Function as Argument -from DOM Deep Look Function part-02;
+
+    // Return of function 
 
         //  return -review again the return function
 
@@ -98,5 +100,72 @@ console.log(calculator.increase(50, 5));
         
         console.log(someThing());
 
-// Function as Argument
+            /* why do we return a function*/
 
+            function double(x) {
+                return x * 2;
+            };
+            function triple(x) {
+                return x * 3;
+            };
+            function fourth(x) {
+                return x * 4;
+            };
+
+            console.log(fourth(5)) // line no 105 to 113 is a long term process of function and thats why we use return function system to made it short and easy code.
+
+            function multiplier(a) {
+                return function(b) {
+                    return b * a;
+                };
+            };
+
+            const double1 = multiplier(2); // double1 = 5 and multiplier = 2 OR a = 2 and b = 5; And line no. 123 is  function Expression;
+            const triple1 = multiplier(3); // triple1 = 5 and multiplier = 3 OR a = 3 and b = 5;
+
+            console.log(triple1(5));
+            
+        // callback function
+
+        /*simple example*/
+        function greeting() {
+            console.log('I will show a simple callback function')
+        };
+
+        function myFunction(callback) {  // myFunction's callback = greeting;
+            callback();
+        };
+
+        myFunction(greeting);
+
+        //example without callback//
+
+         function calculations(num1, num2, action) {
+             if(action === 'add') {
+                 return num1 + num2;
+             } else if(action === 'minus') {
+                 return num1 - num2;
+             }
+         }
+
+         console.log(calculations(10, 5, 'add')); // explanation: here num1 = 10; num1 = 5 and action = add or minus;
+
+          //example with callback//
+
+          const adds = function(a, b) {
+              return a + b;
+          };
+
+          const minuss = function(a, b) {
+            return a - b;
+        };
+
+        const multiplys = function(a, b) {
+            return a * b;
+        };
+
+        function calculators(num3, num4, callback) {
+            return callback(num3, num4);
+        };
+
+        console.log(calculators(20, 5, adds))
